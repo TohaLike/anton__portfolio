dbfd9c221674ef120c08e9322f2adcdd3ec7a8a6
Anton <thetoxalike444@gmail.com>
2023-12-08 12:27:46 +0300

    added new details to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,25 @@
 import React from "react"
 import { useSpring, useTrail, animated, SpringValues } from '@react-spring/web'
-import LogoAnton from "../images/Anton.png"
+import AntonLogo from "../images/Anton.png"
 
 const Header: React.FC = () => {
-   const items = ['Anton Malyshev', 'Frontend-developer', 'BORN: August 6, 2003', 'EMAIL: antonpaper500@gmail.com']
+   const items = ['Anton Malyshev', 'Frontend-developer', 'My work it is developing web-apps and creating UI']
+   const itemsLinks = ['GitHub', 'Telegram', 'Twitter', 'Instagram']
+   const linkBoks = ['https://github.com/TohaLike', 'https://t.me/smokeonthetower']
 
    const fadeInImage = useSpring({
       opacity: 1,
       from: { opacity: 0 },
       config: { duration: 700 },
-      onFrame: (props: SpringValues) => console.log(props),
-
    })
 
    const trailHeader = useTrail(items.length, {
+      opacity: 1,
+      from: { opacity: 0 },
+      config: { duration: 700 }
+   })
+
+   const trailHeaderLinks = useTrail(itemsLinks.length, {
       opacity: 1,
       from: { opacity: 0 },
       config: { duration: 700 }
@@ -24,7 +30,7 @@ const Header: React.FC = () => {
          <div className="visibility">
             <animated.div style={fadeInImage}>
                <div className="visibility__logo__container">
-                  <img className="visibility__logo" src={LogoAnton} />
+                  <img className="visibility__logo" src={AntonLogo} alt="test" />
                </div>
             </animated.div>
 
@@ -33,6 +39,14 @@ const Header: React.FC = () => {
                   <animated.div key={index} style={style} className={`text__content__visibility__${index}`}>
                      {items[index]}
                   </animated.div>
+               ))}
+            </div>
+
+            <div className="visibility__links">
+               {trailHeaderLinks.map((style, index) => (
+                  <animated.a href={linkBoks[index]} key={index} style={style} className={`link__block links__content__visibility__${index}`}>
+                     {itemsLinks[index]}
+                  </animated.a>
                ))}
             </div>
          </div>
